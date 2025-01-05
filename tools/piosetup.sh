3e5961bb199e3db687587bccbb27feed5a2e2f73
Matthew Elgert <mdelgert@yahoo.com>
2025-01-05 13:00:00 -0500

    Add piosetup.sh script to link PlatformIO binaries to ~/.local/bin

---
+++ tools/piosetup.sh
@@ -0,0 +1,6 @@
+# Links pio files to ~/.local/bin
+
+mkdir -p ~/.local/bin  # Ensure the directory exists
+ln -s ~/.platformio/penv/bin/platformio ~/.local/bin/platformio
+ln -s ~/.platformio/penv/bin/pio ~/.local/bin/pio
+ln -s ~/.platformio/penv/bin/piodebuggdb ~/.local/bin/piodebuggdb
